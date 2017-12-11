dfdc34effe42cc45f146f8807691957bc5ebbdea
Kevin Park <kevinpark@Kevins-MBP.fios-router.home>
2017-12-11 01:40:12 -0500

    Adding examples of enum and pure.

--- degree.sol
+++ degree.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.0;
 contract Degree {
 
+    enum LetterGrade { A, AMinus, BPlus, B, BMinus, CPlus, C, CMinus, DPlus, D, DMinus, F }
+
     address student;
     mapping(bytes32 => uint8) courseGrades;
     bytes32[] requiredCourses;
@@ -22,6 +24,35 @@ contract Degree {
                 return false;
             }
         return true;
+    }
+
+    // Convert numeric score to LetterGrade
+    function getLetterGrade(uint8 numGrade) public pure returns (LetterGrade letterGrade) {
+        if (numGrade > 92) {
+            return LetterGrade.A;
+        } else if (numGrade > 89) {
+            return LetterGrade.AMinus;
+        } else if (numGrade > 86) {
+            return LetterGrade.BPlus;
+        } else if (numGrade > 82) {
+            return LetterGrade.B;
+        } else if (numGrade > 79) {
+            return LetterGrade.BMinus;
+        } else if (numGrade > 76) {
+            return LetterGrade.CPlus;
+        } else if (numGrade > 72) {
+            return LetterGrade.C;
+        } else if (numGrade > 69) {
+            return LetterGrade.CMinus;
+        } else if (numGrade > 66) {
+            return LetterGrade.DPlus;
+        } else if (numGrade > 62) {
+            return LetterGrade.D;
+        } else if (numGrade > 59) {
+            return LetterGrade.DMinus;
+        } else {
+            return LetterGrade.F;
+        }
     }
 
 }
